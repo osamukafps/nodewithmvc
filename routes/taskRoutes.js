e06c490c65cef7369da0c2c3817a3a72b0b36fef
Samuel Hipólito <samuelhplt@gmail.com>
2022-09-19 22:43:22 -0300

    Update e delete das tasks

--- routes/taskRoutes.js
+++ routes/taskRoutes.js
@@ -5,5 +5,10 @@ const TaskController = require('../controllers/TaskController');
 
 router.get('/add', TaskController.createTask);
 router.get('/', TaskController.showTasks);
+router.get('/edit/:id', TaskController.updateTask);
+
+router.post('/add', TaskController.createTaskSave);
+router.post('/remove', TaskController.removeTask);
+router.post('/edit', TaskController.updateTaskSave);
 
 module.exports = router;
